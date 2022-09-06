962b75e2fff04e1406ea1c2b2a33b040cb00d9a8
yuexiaoyouling <862668270@qq.com>
2022-09-07 00:07:48 +0800

    first commit

---
+++ src/main/java/com/atMrChen/utils/SqlSessionUtil.java
@@ -0,0 +1,27 @@
+package com.atMrChen.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlSessionUtil {
+        public  static SqlSession getSqlSession(){
+            SqlSession sqlSession=null;
+            try {
+
+                InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+                SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+                sqlSession = sqlSessionFactory.openSession(true);
+                return sqlSession;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+}
